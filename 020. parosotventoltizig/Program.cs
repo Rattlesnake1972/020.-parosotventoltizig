37823e72584358f9c7a563a4d99b44dab725cbe4
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-26 16:06:10 +0200

    Páros számok 50-től 10-ig

---
+++ 020. parosotventoltizig/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _020.parosotventoltizig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 50; i > 9; i--)
+
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            Console.ReadKey();
+        }
+    }
+}
